4f443588781639df7a2e3fdeee5afbafe51fe96c
NargizaMusaeva01 <musaevanargiza1990@gmail.com>
2022-08-23 15:14:23 +0600

    hw13

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Counter from './components/Counter'
+import ClassCounter from './components/ClassCounter'
+
 import './App.css';
 
 function App() {
+ const [likes, setLikes] = useState(5)
+ const [value, setValue] = useState('text')
+
+
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {/* <Counter/> */}
+    <ClassCounter/>
+
+  
     </div>
   );
 }
